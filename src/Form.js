b98534d19e3cd380d56832b1db304cedad6b5cff
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2021-09-14 17:13:56 -0400

    refactor Form component

--- src/Form.js
+++ src/Form.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Form extends Component {
-    constructor() {
-        super();
-        this.state = {
-            userSelection: ""
-        }
-    }
-    handleChange = event => {
-        this.setState({
-            userSelection: event.target.value
-        })
-    }
-    render() {
-        return (
-            <form onClick={() => this.props.userSelection(this.state.userSelection)} className="wrapper">
-                <label htmlFor="country">Select a country to view cases:</label>
-                <select onChange={this.handleChange} name="country" id="country">
-                    {
-                        this.props.countrySelection.map((value, index) => {
-                            return <option key={index} value={value}>{value}</option>
-                        })
-                    }
-                </select>
-            </form>
-        )
-    }
+const Form = props => {
+    return (
+        <form className="wrapper">
+            <label htmlFor="country">Select a country to view cases:</label>
+            <select onChange={(event) => props.userSelection(event.target.value)} name="country" id="country">
+                {
+                    props.countrySelection.map((value, index) => {
+                        return <option key={index} value={value}>{value}</option>
+                    })
+                }
+            </select>
+        </form>
+    )
 }
- 
+
 export default Form;
